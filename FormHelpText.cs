5206e0a93144ab21d8c8959c16e00e2717883e6f
Dave <dsmail@sigenix.com>
2017-08-07 08:00:58 -0400

    Inserted trial Spice logic DLL for review

--- FormHelpText.cs
+++ FormHelpText.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 
+
 namespace DDB
 {
     public partial class FormHelpText : Form
@@ -11,12 +12,10 @@ namespace DDB
         {
             InitializeComponent();
             this.ddbHelpObject = ddbHelpObject;
-            textBox1.Text = ddbHelpObject.GetHelpText();
-        }
+         }
 
         private void btnAccept_Click(object sender, EventArgs e)
         {
-            ddbHelpObject.SaveHelpText(textBox1.Text);
             Close();
         }
 
